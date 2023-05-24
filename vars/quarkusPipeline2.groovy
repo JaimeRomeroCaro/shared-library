c8b49f4f9534963e59e2768665e934ab4b4bee38
JaimeRomeroCaro <129301755+JaimeRomeroCaro@users.noreply.github.com>
2023-05-24 15:45:14 +0200

    quarkusPipeline2.groovy created

---
+++ vars/quarkusPipeline2.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Descargar código fuente') {
+            steps {
+                git branch: 'master', url: 'https://github.com/quarkusio/quarkus'
+            }
+        }
+        
+        stage('Modificar versión en pom.xml') {
+            steps {
+                bat """
+                    cd quarkus
+                    sed -i 's/999-SNAPSHOT/999/g' pom.xml
+                """
+            }
+        }
+
+        stage('Compilar y construir') {
+            steps {
+                bat 'mvn clean install'
+            }
+        }
+
+    }
+}
