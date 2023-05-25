dbca13313596ef609ddfd5489cf56119b33162f2
JaimeRomeroCaro <129301755+JaimeRomeroCaro@users.noreply.github.com>
2023-05-25 09:22:30 +0200

    Update quarkusPipeline4.groovy

--- vars/quarkusPipeline4.groovy
+++ vars/quarkusPipeline4.groovy
@@ -1,7 +1,10 @@
 def call() {
-    // Descargar el código fuente de la rama master de Quarkus desde GitHub
-    checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/quarkusio/quarkus']]]
+    node {
+        // Descargar el código fuente de la rama master de Quarkus desde GitHub
+        checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/quarkusio/quarkus']]]
 
-    // Modificar la versión en el archivo pom.xml
-    sh 'sed -i \'s/999-SNAPSHOT/999/\' pom.xml'
+        // Modificar la versión en el archivo pom.xml
+        sh 'sed -i \'s/999-SNAPSHOT/999/\' pom.xml'
+    }
 }
+
